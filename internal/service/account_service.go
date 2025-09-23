4b74ded9ce42adbb72aa14f0036a225ff6bd798f
itallominatti <itallo.monteiro@propig.com.br>
2025-09-23 08:57:03 -0300

    Add account DTO and service for account creation

---
+++ internal/service/account_service.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
+
+type AccountService struct {
+	repository domain.AccountRepository
+}
+
+func NewAccountService(repository domain.AccountRepository) *AccountService {
+	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+
+}
